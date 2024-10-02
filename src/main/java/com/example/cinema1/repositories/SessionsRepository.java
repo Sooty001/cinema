4da053a4706cfb51a78402fe1dbcea20db9a9f07
Sooty001 <goodg0311@gmail.com>
2024-10-02 23:48:44 +0300

    deleted SessionsRepository

--- src/main/java/com/example/cinema1/repositories/SessionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cinema1.repositories;
-
-import com.example.cinema1.domain.Sessions;
-import java.util.List;
-
-public interface SessionsRepository {
-    List<Sessions> findSessionsByUserId(int userId);
-}
